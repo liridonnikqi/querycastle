e07a3f79526c89604977cfe0fd077bcd35060475
Djelal Fida <djelal.fida@hotmail.com>
2026-09-05 03:43:46 +0200

    refactor v2

--- src/lib/components/ui/MssqlIcon.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	let { size = 22, class: className = '' }: { size?: number; class?: string } = $props();
-</script>
-
-<svg viewBox="0 0 24 24" width={size} height={size} class={className} aria-hidden="true">
-	<path fill="#1377b9" d="M4 5.5 12 3l8 2.5v3L12 11 4 8.5v-3Zm0 5.4 8 2.5 8-2.5v3L12 16.4 4 13.9v-3Zm0 5.4 8 2.5 8-2.5v3L12 21l-8-2.7v-2Z"/>
-</svg>
